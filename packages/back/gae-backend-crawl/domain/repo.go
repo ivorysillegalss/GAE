952d98ebd1378a60fd2f079e76f6c01380f80423
ivorysillegalss <chenzczczc@gmail.com>
2024-11-01 10:36:26 +0800

    feat: time type convert 2 int

--- packages/back/gae-backend-crawl/domain/repo.go
+++ packages/back/gae-backend-crawl/domain/repo.go
@@ -3,7 +3,6 @@ package domain
 import (
 	"github.com/google/go-github/github"
 	"strconv"
-	"time"
 )
 
 type RepositoryValue struct {
@@ -11,8 +10,8 @@ type RepositoryValue struct {
 	OwnerName        string
 	OwnerId          string
 	Name             string
-	CreatedAt        *time.Time
-	UpdatedAt        *time.Time
+	CreatedAt        int64
+	UpdatedAt        int64
 	ForksCount       int
 	NetworkCount     int
 	OpenIssuesCount  int
@@ -34,8 +33,8 @@ func NewRepositoryValue(repo *github.Repository) *RepositoryValue {
 		OwnerName:        *repo.Owner.Name,
 		OwnerId:          strconv.FormatInt(*repo.Owner.ID, 10),
 		Name:             *repo.Name,
-		CreatedAt:        &repo.CreatedAt.Time,
-		UpdatedAt:        &repo.UpdatedAt.Time,
+		CreatedAt:        repo.CreatedAt.Time.Unix(),
+		UpdatedAt:        repo.UpdatedAt.Time.Unix(),
 		ForksCount:       *repo.ForksCount,
 		NetworkCount:     *repo.NetworkCount,
 		OpenIssuesCount:  *repo.OpenIssuesCount,
